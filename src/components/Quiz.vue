8cb7169837b582a29815723cfc312cff9e6c48ff
GabrielNathanael <gabriel@student.undiksha.ac.id>
2023-10-09 20:59:55 +0800

    pesan commit

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,49 +1,80 @@
 <template>
-    <div class="p-4 sm:p-8"> <!-- Add padding to the entire container, increase padding on small screens (sm) -->
-      <div v-if="currentQuestionIndex < questions.length" class="text-center">
-        <h1 class="text-3xl sm:text-4xl font-semibold mb-4">HTML Quiz</h1> <!-- Increase font size on small screens -->
+  <container>
+    <div id="hero" class="p-4 sm:p-8 mx-auto max-w-lg">
+      <!-- Tengah dengan lebar maksimum -->
+      <!-- Add padding to the entire container, increase padding on small screens (sm) -->
+      <div v-if="currentQuestionIndex < questions.length" class="text-left">
+        <h1 class="text-3xl sm:text-4xl font-semibold mb-4">HTML Quiz</h1>
+        <!-- Increase font size on small screens -->
         <p class="mb-2">Question {{ currentQuestionIndex + 1 }}:</p>
-        <p class="text-lg sm:text-xl mb-4">{{ questions[currentQuestionIndex].question }}</p> <!-- Increase font size on small screens -->
+        <p class="text-lg sm:text-xl mb-4">
+          {{ questions[currentQuestionIndex].question }}
+        </p>
+        <!-- Increase font size on small screens -->
         <ul>
-            <li
-              v-for="(option, index) in questions[currentQuestionIndex].options"
-              :key="index"
-              class="mb-2"
+          <li
+            v-for="(option, index) in questions[currentQuestionIndex].options"
+            :key="index"
+            class="mb-2"
+          >
+            <input
+              type="radio"
+              :id="'option' + index"
+              :value="option"
+              v-model="selectedAnswer"
+              class="mr-2"
+            />
+            <label
+              :for="'option' + index"
+              class="text-base"
+              :class="{
+                'bg-green-500 text-white font-semibold p-2 rounded':
+                  selectedAnswer === option,
+              }"
             >
-              <input
-                type="radio"
-                :id="'option' + index"
-                :value="option"
-                v-model="selectedAnswer"
-                class="mr-2"
-              />
-              <label :for="'option' + index" class="text-base" :class="{'bg-green-500 text-white font-semibold p-2 rounded': selectedAnswer === option}">
-                {{ option }}
-              </label>
-            </li>
-          </ul>
-        <div class="space-x-4 mt-4"> <!-- Add space between buttons -->
-          <button @click="goBack" :disabled="currentQuestionIndex === 0" class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded">
+              {{ option }}
+            </label>
+          </li>
+        </ul>
+        <div class="space-x-4 mt-4">
+          <!-- Add space between buttons -->
+          <button
+            @click="goBack"
+            :disabled="currentQuestionIndex === 0"
+            class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded"
+          >
             Back
           </button>
-          <button @click="saveAnswer" class="bg-green-500 hover:bg-green-700 text-white font-semibold py-2 px-4 rounded">
+          <button
+            @click="saveAnswer"
+            class="bg-green-500 hover:bg-green-700 text-white font-semibold py-2 px-4 rounded"
+          >
             Save Answer
           </button>
-          <button @click="checkAnswer" class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded">
+          <button
+            @click="checkAnswer"
+            class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded"
+          >
             Next
           </button>
         </div>
       </div>
-      <div v-else class="text-left"> <!-- Left-align content -->
-        <h1 class="text-3xl sm:text-4xl font-semibold mb-4">Quiz Completed!</h1> <!-- Increase font size on small screens -->
+      <div v-else class="text-left">
+        <!-- Left-align content -->
+        <h1 class="text-3xl sm:text-4xl font-semibold mb-4">Quiz Completed!</h1>
+        <!-- Increase font size on small screens -->
         <p class="mb-2">Your Score: {{ score }} / {{ questions.length }}</p>
       </div>
     </div>
-  </template>
-  
+  </container>
+</template>
 
-  
-
+<style>
+body {
+  @apply bg-yellow-500;
+  font-family: "Poppins", sans-serif;
+}
+</style>
 <script>
 export default {
   data() {
